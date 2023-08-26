a86020b013cdfa5677a632056fb0e58623ad85c9
kunalgaurav <kunalgaurav1420@gmail.com>
2023-08-26 11:44:47 +0530

    javascript part 1

---
+++ file1.js
@@ -0,0 +1,31 @@
+const  name = "kunal";
+var surname = "gaurav";
+
+
+console.log(a);
+var a = 5;
+
+var a = [1,2 ,3,4,5];
+
+var b = a;
+
+b.pop();
+
+
+var kunal = {
+    color: "fair",
+    height : "6ft",
+    address : "bihar",
+    college : "gne",
+    
+    //this is called method
+    koifunction : function(){
+
+    }
+
+}
+
+
+
+
+
